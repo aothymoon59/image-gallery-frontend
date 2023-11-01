c896c74aefa6669530112fb25f79d0f410e3c93d
aothymoon59 <aothymoon59@gmail.com>
2023-11-01 14:40:21 +0600

    initial project setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Container from "./Components/Container/Container";
+
+const App = () => {
+  return (
+    <div>
+      <Container>
+        hello image gallery
+      </Container>
+    </div>
+  );
+};
+
+export default App;

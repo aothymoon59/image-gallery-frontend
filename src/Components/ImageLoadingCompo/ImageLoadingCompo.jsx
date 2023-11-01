71cc9ef21c292fdb27ee5f04cddde10188310368
aothymoon59 <aothymoon59@gmail.com>
2023-11-02 03:19:59 +0600

    manage loading state when data is loading

---
+++ src/Components/ImageLoadingCompo/ImageLoadingCompo.jsx
@@ -0,0 +1,22 @@
+import { LineWave } from 'react-loader-spinner';
+
+const ImageLoadingCompo = () => {
+    return (
+        <div className='w-full flex justify-center items-center min-h-[40vh]'>
+            <LineWave
+                height="100"
+                width="100"
+                color=" #0000FF"
+                ariaLabel="line-wave"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                firstLineColor=""
+                middleLineColor=""
+                lastLineColor=""
+            />
+        </div>
+    );
+};
+
+export default ImageLoadingCompo;
--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -4,24 +4,19 @@ import { useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import toast from "react-hot-toast";
 import { BiImage } from "react-icons/bi";
+import ImageLoadingCompo from "../../Components/ImageLoadingCompo/ImageLoadingCompo";
 
 const Gallery = () => {
-    const [finalGalleryData, setFinalGalleryData] = useState([])
     const [galleryData, setGalleryData] = useState([])
     const [control, setControl] = useState(true);
     const [imageUploadLoading, setImageUploadLoading] = useState(false)
     const [imageLoading, setImageLoading] = useState(true)
     const [selectedImage, setSelectedImage] = useState([])
 
-    // useEffect(() => {
-    //     // Update galleryData when finalGalleryData changes
-    //     setGalleryData(finalGalleryData);
-    // }, [finalGalleryData]);
-
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get('http://localhost:5000/get-gallery-images');
+                const response = await axios.get('https://image-gallery-server.vercel.app/get-gallery-images');
                 // const checkedImages = response.data.filter(data => data?.isChecked === true);
                 // setSelectedImage(checkedImages);
                 setGalleryData(response.data);
@@ -83,59 +78,63 @@ const Gallery = () => {
             <div className="p-5 border-b">
                 <h3 className="text-xl font-bold">Gallery</h3>
             </div>
-            <DragDropContext onDragEnd={handleOnDragEnd}>
-                <Droppable droppableId="gallery">
-                    {
-                        (provided) => (
-                            <div className="p-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5" {...provided.droppableProps} ref={provided.innerRef}>
-                                {galleryData?.map((item, index) => {
-                                    return <Draggable key={item?._id} draggableId={item?._id} index={index}>
-                                        {
-                                            (provided) => (
-                                                // Check if it's the first image to apply different styling
-                                                <div className={`${index === 0 && 'col-span-2 row-span-2'}`} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                                                    {/* single image component start  */}
-                                                    <div className={`group border-2 rounded-xl overflow-hidden relative transition duration-200 transform`}>
-                                                        <img src={item?.thumb} className="w-full h-full" alt="Gallery Image" />
-                                                        <div className="bg-black rounded-xl bg-opacity-75 opacity-0 hover:opacity-100 text-blue-100 font-medium p-2 absolute inset-0 transition duration-300 ease-in-out">
-                                                            <input type="checkbox" className='w-5 h-5 rounded-md absolute top-[7%] left-[7%]' name="" id="" />
-                                                        </div>
-                                                    </div>
-                                                    {/* single image component end  */}
-
-                                                </div>
-                                            )
-                                        }
-
-                                    </Draggable>;
-                                })}
-                                {provided.placeholder}
-                                {/* image upload start */}
-                                <div className="col-span-1 row-span-1 h-full min-h-[210px]">
-                                    <label htmlFor="file-upload" className="cursor-pointer">
-                                        <input
-                                            id="file-upload"
-                                            type="file"
-                                            className="hidden"
-                                            name="image"
-                                            onChange={handleFileChange}
-                                        />
-                                        <div className="border-2 border-dashed border-gray-300 p-6 rounded-lg h-full min-h-[210px] flex flex-col items-center justify-center">
-                                            <BiImage className="text-3xl" />
-                                            <p className="text-xl font-semibold text-gray-700 text-center">Add Images</p>
-
+            {
+                imageLoading ? <ImageLoadingCompo /> : <DragDropContext onDragEnd={handleOnDragEnd}>
+                    <Droppable droppableId="gallery">
+                        {
+                            (provided) => (
+                                <div className="p-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5" {...provided.droppableProps} ref={provided.innerRef}>
+                                    {galleryData?.map((item, index) => {
+                                        return <Draggable key={item?._id} draggableId={item?._id} index={index}>
                                             {
-                                                imageUploadLoading ? <p className="text-xs mt-2 text-center">Uploading...</p> : ''
+                                                (provided) => (
+                                                    // Check if it's the first image to apply different styling
+                                                    <div className={`${index === 0 && 'col-span-2 row-span-2'}`} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                                                        {/* single image component start  */}
+                                                        <div className={`group border-2 rounded-xl overflow-hidden relative transition duration-200 transform`}>
+                                                            <img src={item?.thumb} className="w-full h-full" alt="Gallery Image" />
+                                                            <div className="bg-black rounded-xl bg-opacity-75 opacity-0 hover:opacity-100 text-blue-100 font-medium p-2 absolute inset-0 transition duration-300 ease-in-out">
+                                                                <input type="checkbox" className='w-5 h-5 rounded-md absolute top-[7%] left-[7%]' name="" id="" />
+                                                            </div>
+                                                        </div>
+                                                        {/* single image component end  */}
+
+                                                    </div>
+                                                )
                                             }
-                                        </div>
-                                    </label>
+
+                                        </Draggable>;
+                                    })}
+                                    {provided.placeholder}
+                                    {/* image upload start */}
+                                    <div className="col-span-1 row-span-1 h-full min-h-[210px]">
+                                        <label htmlFor="file-upload" className="cursor-pointer">
+                                            <input
+                                                id="file-upload"
+                                                type="file"
+                                                className="hidden"
+                                                name="image"
+                                                onChange={handleFileChange}
+                                            />
+                                            <div className="border-2 border-dashed border-gray-300 p-6 rounded-lg h-full min-h-[210px] flex flex-col items-center justify-center">
+                                                <BiImage className="text-3xl" />
+                                                <p className="text-xl font-semibold text-gray-700 text-center">Add Images</p>
+
+                                                {
+                                                    imageUploadLoading ? <p className="text-xs mt-2 text-center">Uploading...</p> : ''
+                                                }
+                                            </div>
+                                        </label>
+                                    </div>
+                                    {/* image upload end */}
                                 </div>
-                                {/* image upload end */}
-                            </div>
-                        )
-                    }
-                </Droppable>
-            </DragDropContext>
+                            )
+                        }
+                    </Droppable>
+                </DragDropContext>
+            }
+
+
         </div>
     );
 };

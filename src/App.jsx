3e8d333cda37586283c7a172bb59b7f87daef9ef
aothymoon59 <aothymoon59@gmail.com>
2023-11-06 10:38:02 +0600

    final push

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Container from "./Components/Container/Container";
 import Gallery from "./Pages/Gallery/Gallery";
-import Gallery2 from "./Pages/Gallery/Gallery2";
 
 const App = () => {
   return (
     <div>
       <Container>
         <Gallery />
-        {/* <Gallery2 /> */}
       </Container>
     </div>
   );
